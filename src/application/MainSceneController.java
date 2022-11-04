5538269a897da3f61bf2a862c771fa9c8dc7e66d
EmilySchrag <emily.schrag@ucalgary.ca>
2022-11-04 17:44:22 +0000

    Finished scene change path for the sleep habit

--- src/application/MainSceneController.java
+++ src/application/MainSceneController.java
@@ -81,26 +81,66 @@ public class MainSceneController {
     	Label sleepLabel = new Label("Enter your sleep goal");
     	Button toSleepGoal = new Button("Enter");
     	
-    	//create the sleepGoal Scene
-//    	VBox sleepGoalContainer = new VBox(10);
-//    	
-//    	HBox enterSleepGoal = new HBox();
-//    	Label enterSleepGoalLabel = new Label("Enter your sleep goal");
-//    	Label hourLabel = new Label("Hour");
-//    	ChoiceBox hourChoiceBox = new ChoiceBox();
-//    	Label minuteLabel = new Label("Minute");
-//    	ChoiceBox minuteChoiceBox = new ChoiceBox();
-//    	Label printSleepGoal = new Label("");
-//    	Button doneButton = new Button("Done");
-//    	
-//    	enterSleepGoal.getChildren().addAll(enterSleepGoalLabel, hourLabel, hourChoiceBox, minuteLabel, minuteChoiceBox);
-//    	sleepGoalContainer.getChildren().addAll(enterSleepGoal, printSleepGoal, doneButton);
-   	Scene sleepGoalScene = new Scene(new Label("place"));
-    	//the enter takes you to the sleep goal scene
-    	toSleepGoal.setOnAction(toSleepGoalEvent -> applicationStage.setScene(sleepGoalScene));
+    		//create the sleepGoal Scene
+    		VBox sleepGoalContainer = new VBox(10);
     	
+    		HBox enterSleepGoal = new HBox(5);
+    		Label enterSleepGoalLabel = new Label("Enter your sleep goal");
+    		Label hourLabel = new Label("Hour");
+    		ChoiceBox hourChoiceBox = new ChoiceBox();
+    		Label minuteLabel = new Label("Minute");
+    		ChoiceBox minuteChoiceBox = new ChoiceBox();
+    		Label printSleepGoal = new Label("Your sleep goal: ");
     	
-    	sleepGoalButtonContainer.getChildren().addAll(sleepLabel, toSleepGoal);
+    		//Done Button on sleep goal scene to take user to sleepdata scene
+    		Button doneButton = new Button("Done");
+   
+    	
+    		enterSleepGoal.getChildren().addAll(hourLabel, hourChoiceBox, minuteLabel, minuteChoiceBox);
+    		sleepGoalContainer.getChildren().addAll(enterSleepGoalLabel, enterSleepGoal, printSleepGoal, doneButton);
+    		Scene sleepGoalScene = new Scene(sleepGoalContainer);
+    		//the enter takes you to the sleep goal scene
+    		toSleepGoal.setOnAction(toSleepGoalEvent -> applicationStage.setScene(sleepGoalScene));
+    	
+    			//create the sleepData Scene
+    			VBox sleepDataContainer = new VBox(10);
+    		
+    			Label bedtimeLabel = new Label("What time did you go to bed?");
+    		
+    			HBox bedtimeContainer = new HBox(5);
+    			Label hourLabel2 = new Label("Hour");
+    			ChoiceBox hourChoiceBox2 = new ChoiceBox();
+    			Label minuteLabel2 = new Label("Minute");
+    			ChoiceBox minuteChoiceBox2 = new ChoiceBox();
+    			Label ampmLabel = new Label("AM/PM");
+    			ChoiceBox ampmChoiceBox = new ChoiceBox();
+    			bedtimeContainer.getChildren().addAll(hourLabel2, hourChoiceBox2, minuteLabel2, minuteChoiceBox2, ampmLabel, ampmChoiceBox);
+    		
+    			Label waketimeLabel = new Label("What time did you wake up?");
+    		
+    			HBox waketimeContainer = new HBox(5);
+    			Label hourLabel3 = new Label("Hour");
+    			ChoiceBox hourChoiceBox3 = new ChoiceBox();
+    			Label minuteLabel3 = new Label("Minute");
+    			ChoiceBox minuteChoiceBox3 = new ChoiceBox();
+    			Label ampmLabel2 = new Label("AM/PM");
+    			ChoiceBox ampmChoiceBox2 = new ChoiceBox();
+    			waketimeContainer.getChildren().addAll(hourLabel3, hourChoiceBox3, minuteLabel3, minuteChoiceBox3, ampmLabel2, ampmChoiceBox2);
+    		
+    			Button calculateSleep = new Button("Calculate");
+    		
+    			Label printSleep = new Label("PRINT SLEEP AMOUNT HERE: ");
+    			Label metGoalInfo = new Label("PRINT GOAL MET INFO HERE: ");
+    		
+    			Button doneButton2 = new Button("Done");
+    			doneButton2.setOnAction(doneEvent2 -> applicationStage.setScene(mainScene));
+    		
+    		
+    			sleepDataContainer.getChildren().addAll(bedtimeLabel, bedtimeContainer, waketimeLabel, waketimeContainer, calculateSleep, printSleep, metGoalInfo, doneButton2);
+    			Scene sleepDataScene = new Scene(sleepDataContainer);
+    			doneButton.setOnAction(doneEvent -> applicationStage.setScene(sleepDataScene));
+    		
+    		sleepGoalButtonContainer.getChildren().addAll(sleepLabel, toSleepGoal);
     	
     	//back button
     	Button backButton = new Button("Back");
